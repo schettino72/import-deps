65e03d29292e5f91c5289afb637e7fef3f429ff7
Sebastian Pipping <sebastian@pipping.org>
2024-05-02 23:53:41 +0200

    __main__.py: Add --help and --version, and hide symbol "main"

--- import_deps/__main__.py
+++ import_deps/__main__.py
@@ -1,10 +1,16 @@
+import argparse
 import sys
 
-from . import PyModule, ModuleSet
+from . import __version__, PyModule, ModuleSet
 
 
-def main(module_path):
-    module = PyModule(module_path)
+def _main(argv):
+    parser = argparse.ArgumentParser(prog='import_deps')
+    parser.add_argument('module_path', metavar='PATH/TO/FILE.py', help='Python file to analyze')
+    parser.add_argument('--version', action='version', version='.'.join(str(i) for i in __version__))
+    config = parser.parse_args(argv[1:])
+
+    module = PyModule(config.module_path)
     base_path = module.pkg_path().resolve()
     mset = ModuleSet(base_path.glob('**/*.py'))
     imports = mset.get_imports(module, return_fqn=True)
@@ -12,4 +18,4 @@ def main(module_path):
     sys.exit(0)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    _main(sys.argv)
