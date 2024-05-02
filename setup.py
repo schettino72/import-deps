302fd9af6914a6bd8575b03892146eae0f2abf2d
Sebastian Pipping <sebastian@pipping.org>
2024-05-02 23:22:05 +0200

    Add support for Python 3.11 and 3.12

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Quality Assurance',
     ),
